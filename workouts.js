93776d9be5a16cf63faa2768f700046f89df725c
Daniel K. Guolo <daniel_kv@hotmail.com>
2024-10-16 21:17:52 -0300

    init

--- workouts.js
+++ workouts.js
@@ -1,21 +1,37 @@
-import wod1Masc from "./wod1-masc.js";
 import wod1Fem from "./wod1-fem.js";
-import wod2Masc from "./wod2-masc.js";
+import wod1Masc from "./wod1-masc.js";
 import wod2Fem from "./wod2-fem.js";
-import wod3Masc from "./wod3-masc.js";
+import wod2Masc from "./wod2-masc.js";
 import wod3Fem from "./wod3-fem.js";
+import wod3Masc from "./wod3-masc.js";
+import wod4Fem from "./wod4-fem.js";
 import wod4Masc from "./wod4-masc.js";
-import wod4Fem from "./wod4-masc.js";
+
+function filterCategory(workouts, category, repeat = 1) {
+  const categoryArr = Array.isArray(category) ? category : [category];
+  const filtered = workouts.filter((workout) =>
+    categoryArr.includes(workout.category)
+  );
+  const arr = new Array(repeat).fill(filtered).flat();
+
+  arr.sort((a, b) => a.eventNumber - b.eventNumber);
+
+  return arr;
+}
 
 export default {
-  events: [
-    ...wod1Masc,
-    ...wod1Fem,
-    ...wod2Masc,
-    ...wod2Fem,
-    ...wod3Masc,
-    ...wod3Fem,
-    ...wod4Masc,
-    ...wod4Fem,
-  ],
+  events: filterCategory(
+    [
+      ...wod1Fem,
+      ...wod1Masc,
+      ...wod2Fem,
+      ...wod2Masc,
+      ...wod3Fem,
+      ...wod3Masc,
+      ...wod4Fem,
+      ...wod4Masc,
+    ],
+    ["Scaled Masculino"],
+    5,
+  ),
 };
